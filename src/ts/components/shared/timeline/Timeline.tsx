1c9b1895f50eaf3d9c87065e7a0f935dfe9c4ea2
Adam Morsi <adam.morsi@yahoo.com>
2022-01-26 09:39:24 +0200

    add timeline component

---
+++ src/ts/components/shared/timeline/Timeline.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react';
+//styles
+import classes from './Timeline.scss';
+import TimelineEntry from '../timelineEntry/TimelineEntry';
+import { TimelineInterface } from '../../../interfaces/components/TimelineEntryInterface';
+
+const Timeline: FC<TimelineInterface> = ({ list }) => (
+	<div className={classes.timelineWrapper}>
+		<ul className={classes.timeline}>
+			{list.map((el, i) => (
+				<TimelineEntry
+					key={i}
+					label={el.label}
+					isAccordionOpen={el.isAccordionOpen}
+					accordionIcon={el.accordionIcon}
+					createdAt={el.createdAt}
+					status={el.status}
+				>
+					{el.content}
+				</TimelineEntry>
+			))}
+		</ul>
+	</div>
+);
+
+export default Timeline;
--- src/ts/interfaces/components/TimelineEntryInterface.ts
+++ src/ts/interfaces/components/TimelineEntryInterface.ts
@@ -1,7 +1,13 @@
+import { ReactNode } from 'react';
 //interfaces
 import { AccordionInterface } from './AccordionInterface';
 
 export interface TimelineEntryInterface extends AccordionInterface {
 	createdAt: string;
 	status: string;
+	content?: ReactNode;
+}
+
+export interface TimelineInterface {
+	list: TimelineEntryInterface[];
 }

8b8d6c4e3df416fb571f3ad09546a2a8a6903b41
Adam Morsi <adam.morsi@yahoo.com>
2022-01-26 17:47:22 +0200

    use config in timeline page

--- src/ts/containers/pages/timelinePage/TimelinePage.tsx
+++ src/ts/containers/pages/timelinePage/TimelinePage.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { format, addDays } from 'date-fns';
 //component
 import Timeline from '../../../components/shared/timeline/Timeline';
+import { viewsController } from '../../../constants/Constants';
 
 const TimelinePage: FC = (): JSX.Element => {
 	const addOneDayToDate = (num: number) => format(addDays(new Date(), num), 'd MMMM yyy'),
@@ -37,7 +38,29 @@ const TimelinePage: FC = (): JSX.Element => {
 				content: <p>content of the current timeline entry</p>,
 			},
 		];
-	return <Timeline list={list} />;
+	return (
+		<>
+			<p>
+				Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec
+				pellentesque velit pede quis nunc. Etiam vitae tortor. Etiam ultricies nisi vel augue. Sed
+				mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a
+				orci. Sed in libero ut nibh placerat accumsan.
+			</p>
+
+			<p>
+				Phasellus tempus. Donec sodales sagittis magna. Phasellus dolor. Aenean ut eros et nisl
+				sagittis vestibulum. Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu
+				sollicitudin urna dolor sagittis lacus.
+			</p>
+
+			<p>
+				Vestibulum eu odio. Fusce fermentum. Praesent ut ligula non mi varius sagittis. Vestibulum
+				volutpat pretium libero. Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec,
+				nisi.
+			</p>
+			{viewsController.timelinePage?.timeLine?.show && <Timeline list={list} />}
+		</>
+	);
 };
 
 export default TimelinePage;

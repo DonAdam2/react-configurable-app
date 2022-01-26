b959a0d57046db55fbd1defd278c076111cac5c6
Adam Morsi <adam.morsi@yahoo.com>
2022-01-26 16:57:38 +0200

    use config in home page

--- src/ts/containers/pages/homePage/HomePage.tsx
+++ src/ts/containers/pages/homePage/HomePage.tsx
@@ -1,19 +1,35 @@
-import React, { FC } from 'react';
+import React from 'react';
 // styles
 import classes from './HomePage.scss';
 //components
 import Parallax from '../../../components/shared/parallax/Parallax';
 import PageProgressBar from '../../../components/shared/pageProgressBar/PageProgressBar';
+import { themeOptions, viewsController } from '../../../constants/Constants';
 
-const HomePage: FC = (): JSX.Element => {
+const HomePage = () => {
+	const { homePage } = viewsController;
 	return (
 		<div className={classes.homeWrapper}>
-			<PageProgressBar backgroundColor="green" height=".5rem" />
-			<Parallax imageUrl="https://picsum.photos/1280/500/?image=50" parallaxSpeed={3}>
-				<div className={classes.content}>
-					<h1>Speed (3)</h1>
-				</div>
-			</Parallax>
+			{homePage?.progressBar?.show && (
+				<PageProgressBar
+					backgroundColor={themeOptions.palette.success.main}
+					height={homePage.progressBar.height}
+				/>
+			)}
+			{homePage?.parallax?.show && (
+				<Parallax
+					imageUrl={
+						homePage.parallax.imageUrl
+							? homePage.parallax.imageUrl
+							: 'https://picsum.photos/1280/500/?image=50'
+					}
+					parallaxSpeed={homePage.parallax.speed ? homePage.parallax.speed : 3}
+				>
+					<div className={classes.content}>
+						<h1>Speed ({homePage.parallax.speed})</h1>
+					</div>
+				</Parallax>
+			)}
 			<p>
 				Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec
 				pellentesque velit pede quis nunc. Etiam vitae tortor. Etiam ultricies nisi vel augue. Sed

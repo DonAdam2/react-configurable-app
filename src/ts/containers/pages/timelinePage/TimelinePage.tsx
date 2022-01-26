8b212d96466f5ea5f8db34a3b1c2e0c5659edba6
Adam Morsi <adam.morsi@yahoo.com>
2022-01-26 09:47:36 +0200

    use timeline component in timeline page

--- src/ts/containers/pages/timelinePage/TimelinePage.tsx
+++ src/ts/containers/pages/timelinePage/TimelinePage.tsx
@@ -1,9 +1,43 @@
 import React, { FC } from 'react';
-// styles
-import classes from './TimelinePage.scss';
+import { format, addDays } from 'date-fns';
+//component
+import Timeline from '../../../components/shared/timeline/Timeline';
 
 const TimelinePage: FC = (): JSX.Element => {
-	return <div className={classes.timelineWrapper}>TimelinePage</div>;
+	const addOneDayToDate = (num: number) => format(addDays(new Date(), num), 'd MMMM yyy'),
+		list = [
+			{
+				label: 'Success',
+				createdAt: format(new Date(), 'd MMMM yyy'),
+				status: 'success',
+				content: <p>content of the current timeline entry</p>,
+			},
+			{
+				label: 'Primary',
+				createdAt: addOneDayToDate(1),
+				status: 'primary',
+				content: <p>content of the current timeline entry</p>,
+			},
+			{
+				label: 'Warning',
+				createdAt: addOneDayToDate(2),
+				status: 'warning',
+				content: <p>content of the current timeline entry</p>,
+			},
+			{
+				label: 'Default',
+				createdAt: addOneDayToDate(3),
+				status: 'default',
+				content: <p>content of the current timeline entry</p>,
+			},
+			{
+				label: 'Danger',
+				createdAt: addOneDayToDate(4),
+				status: 'danger',
+				content: <p>content of the current timeline entry</p>,
+			},
+		];
+	return <Timeline list={list} />;
 };
 
 export default TimelinePage;
